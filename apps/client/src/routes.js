4859033ccb4726c23879271fdb34afef2c530ccb
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-09-15 20:31:22 +0700

    fix: rename to jsx
    also remove import from constants

--- apps/client/src/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "@tanstack/react-location";
-import { routesDefinition } from "./helpers/constants";
-
-function Routes() {
-  return <Router routes={routesDefinition} />;
-}
-
-export default Routes;
