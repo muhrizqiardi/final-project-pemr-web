579bd72f885473a833a655e69e0dbdf039967eb8
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-09-17 11:29:09 +0700

    build: update color scheme to tailwind config

--- apps/client/tailwind.config.cjs
+++ apps/client/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          primary: "#E3DD40",
+          secondary: "#C16060",
+          light: {
+            200: "#F5FFBA",
+            100: "#FCFFEB",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
