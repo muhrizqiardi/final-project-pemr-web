cb875f6767ead3c0fca60c72e8fdba0f22ace82e
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-09-29 11:02:44 +0700

    style: remove comments

--- apps/server/src/controllers/jobseekers.js
+++ apps/server/src/controllers/jobseekers.js
@@ -1,21 +1,15 @@
-// mengimpor fungsi prisma yang berada di folder helpers
 const prisma = require("../helpers/prisma");
 
 function jobseekers(fastify, options, done) {
-  // membuat post request di path /jobseekers
   fastify.post("/jobseekers", (request, reply) => {
-    // jika terjadi error akan langsung loncat ke bawah "catch"
     try {
-      // membuat jobseeker baru di database menggunakan prisma
       const newJobseeker = prisma.jobseeker.create({ data: reply.body });
 
-      // mengirim data user baru dengan status code 201
       reply.status(201).send({
         code: 201,
         data: newJobseeker,
       });
     } catch (error) {
-      // mengirimkan response dengan status code 500 (internal server error)
       reply.status(500).send({
         code: 500,
         message: "Gagal membuat jobseeker baru",
@@ -23,9 +17,7 @@ function jobseekers(fastify, options, done) {
     }
   });
 
-  // bisa ditambahkan endpoint-endpoint lain yang berkaitan dengan jobseeker
-
-  done(); // pastikan jalankan fungsi done di paling bawah 
+  done();
 }
 
 module.exports = jobseekers;
