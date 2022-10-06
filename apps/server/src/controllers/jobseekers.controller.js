5634368cfe7d682c02ac3b061f9f6a65e310db35
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-06 09:52:54 +0700

    feat: add role and id_jobseeker

--- apps/server/src/controllers/jobseekers.controller.js
+++ apps/server/src/controllers/jobseekers.controller.js
@@ -1,10 +1,17 @@
 const prisma = require("../helpers/prisma");
+const { v4: uuidV4 } = require("uuid");
 const { createJobseekerSchema } = require("../schemas/jobseeker.schema");
 
 function jobseekers(fastify, options, done) {
   fastify.post("/jobseekers", createJobseekerSchema, async (request, reply) => {
     try {
-      const newJobseeker = await prisma.jobseeker.create({ data: request.body });
+      const newJobseeker = await prisma.jobseeker.create({
+        data: {
+          id_jobseeker: uuidV4(),
+          role: "jobseeker",
+          ...request.body,
+        },
+      });
 
       reply.status(201).send({
         code: 201,
