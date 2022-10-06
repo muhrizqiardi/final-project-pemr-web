5f225a1da3a6f4347dd3480df63a2ffac1768df7
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-06 08:39:09 +0700

    feat: register swaggerConfigs and routes to server

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,3 +1,8 @@
+const swaggerConfigs = require("./helpers/swaggerConfigs");
+const routes = require("./routes");
 const server = require("fastify")({ logger: true });
+
+server.register(require("@fastify/swagger"), swaggerConfigs);
+server.register(routes);
 
 module.exports = server;
