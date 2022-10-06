900b114f6d0dc4a59a3b5fe2f8080e20b6a9754a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-06 08:39:46 +0700

    fix: fix jobseeker schema not appearing in Swagger

--- apps/server/src/schemas/jobseeker.schema.js
+++ apps/server/src/schemas/jobseeker.schema.js
@@ -2,19 +2,21 @@ const { Type } = require("@sinclair/typebox");
 
 const createJobseekerSchema = {
   schema: {
-    username: Type.String({ pattern: "/[A-Za-z0-9_-]+/" }),
-    password: Type.String({ minLength: 8 }),
-    nama_company: Type.String(),
-    gender: Type.String(),
-    alamat: Type.String(),
-    no_hp: Type.String({
-      pattern: "/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$/",
+    body: Type.Object({
+      username: Type.String({ pattern: "/[A-Za-z0-9_-]+/" }),
+      password: Type.String({ minLength: 8 }),
+      nama_company: Type.String(),
+      gender: Type.String(),
+      alamat: Type.String(),
+      no_hp: Type.String({
+        pattern: "/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-s./0-9]*$/",
+      }),
+      email: Type.String({ format: "email" }),
+      pendidikan: Type.String(),
+      skill: Type.String(),
+      pengalaman: Type.String(),
+      profilepicture: Type.String({ format: "uri" }),
     }),
-    email: Type.String({ format: "email" }),
-    pendidikan: Type.String(),
-    skill: Type.String(),
-    pengalaman: Type.String(),
-    profilepicture: Type.String({ format: "uri" }),
   },
 };
 
