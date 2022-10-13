b22c5f58992b5727c2b456395f94dc979e590644
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-13 09:37:01 +0700

    feat: add CRUD for lowongan

--- apps/server/src/routes.js
+++ apps/server/src/routes.js
@@ -1,9 +1,11 @@
 const jobseekers = require("./controllers/jobseekers.controller");
 const jobseekersAuth = require("./controllers/jobseekersAuth.controller");
+const lowonganController = require("./controllers/lowongan.controller");
 
 function routes(fastify, options, done) {
   fastify.register(jobseekers);
   fastify.register(jobseekersAuth);
+  fastify.register(lowonganController);
 
   return done();
 }
